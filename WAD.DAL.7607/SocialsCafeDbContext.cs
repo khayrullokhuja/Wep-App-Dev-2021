dddcc11eb9e868bb7f32952bc10581a52e74d7e0
khayrullokhuja <xayrulla7898@gmail.com>
2021-06-24 21:02:07 +0500

    cafe database context added

---
+++ WAD.DAL.7607/SocialsCafeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WAD.DAL._7607.DBO;
+
+namespace WAD.DAL._7607
+{
+    public class SocialsCafeDbContext : DbContext
+    {
+        public SocialsCafeDbContext(DbContextOptions<SocialsCafeDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Cafe> Cafe { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Department> Department { get; set; }
+
+    }
+}
