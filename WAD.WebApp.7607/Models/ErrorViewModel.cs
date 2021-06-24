2c9336955a6031caab61d742307ca86c0db529ae
khayrullokhuja <xayrulla7898@gmail.com>
2021-06-24 10:25:53 +0500

    empty mvc web application created

---
+++ WAD.WebApp.7607/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAD.WebApp._7607.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
